79ceeb416b67c77941515c18f6f6a9049d1f80d3
Karol Żyłka <zylkakarol@gmail.com>
2023-02-19 17:23:23 +0100

    Deleted code

--- shops.py
+++ shops.py
@@ -1,21 +1,4 @@
 #Zadanie 1 
 
-shops = {
-    "piekarnia": ["chleb", "bułki", "pączek"],   
-    "warzywniak": ["marchew", "seler", "rukola"]
-}
-
-d = 0 
-
-print("Lista zakupów")
-for a, b in shops.items():
-    a = a.capitalize()
-    for c in range(0,len(b)):
-        b[c] = b[c].capitalize()
-        d = d + 1
-    print(f"Idę do {a} i kupuję tam {b}")
-    
-print(f"W sumie kupuję {d} produktów")
-
 
 
