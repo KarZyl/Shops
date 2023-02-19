4154332cc3e1db5ed45cfe5dce36854cb31b0ee6
Karol Żyłka <zylkakarol@gmail.com>
2023-02-19 17:32:03 +0100

    Add function and iteration, capitalize keys and items, print text message

--- shops.py
+++ shops.py
@@ -5,5 +5,17 @@ shops = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+d = 0 
+
+print("Lista zakupów")
+for a, b in shops.items():
+    a = a.capitalize()
+    for c in range(0,len(b)):
+        b[c] = b[c].capitalize()
+        d = d + 1
+    print(f"Idę do {a} i kupuję tam {b}")
+    
+
+
 
 
