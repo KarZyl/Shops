5590a719ccf3a42d8ad01677b2954efffc57c1c1
Karol Żyłka <zylkakarol@gmail.com>
2023-02-19 17:15:10 +0100

    Add shops.py

---
+++ shops.py
@@ -0,0 +1,21 @@
+#Zadanie 1 
+
+shops = {
+    "piekarnia": ["chleb", "bułki", "pączek"],   
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+
+d = 0 
+
+print("Lista zakupów")
+for a, b in shops.items():
+    a = a.capitalize()
+    for c in range(0,len(b)):
+        b[c] = b[c].capitalize()
+        d = d + 1
+    print(f"Idę do {a} i kupuję tam {b}")
+    
+print(f"W sumie kupuję {d} produktów")
+
+
+
